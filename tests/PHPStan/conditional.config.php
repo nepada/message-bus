3fd586c4baebcf141641474cf2ff818e38994cc6
Petr Morávek <petr@pada.cz>
2021-01-15 15:09:53 +0100

    Update PHPStan

--- tests/PHPStan/conditional.config.php
+++ tests/PHPStan/conditional.config.php
@@ -6,7 +6,7 @@ $config = [];
 if (PHP_VERSION_ID >= 8_00_00) {
     // Change of signature in PHP 8.0
     $config['parameters']['ignoreErrors'][] = [
-        'message' => '~Strict comparison using === between array and false will always evaluate to false~',
+        'message' => '~Strict comparison using === between array.* and false will always evaluate to false~',
         'path' => '../../src/MessageBus/Logging/MessageContextResolver.php',
         'count' => 1,
     ];
