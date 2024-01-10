0ce04b4a215436920b17410f615504df2fb6825b
Petr Morávek <petr@pada.cz>
2024-01-10 07:00:38 +0100

    Use marker interface from Symfony Messenger in middleware signature and thrown exceptions

--- tests/PHPStan/conditional.config.php
+++ tests/PHPStan/conditional.config.php
@@ -1,6 +1,17 @@
 <?php
 declare(strict_types = 1);
 
+use Composer\InstalledVersions;
+use Composer\Semver\VersionParser;
+
 $config = [];
+
+if (InstalledVersions::satisfies(new VersionParser(), 'symfony/messenger', '<7.1')) {
+    $config['parameters']['ignoreErrors'][] = [
+        'message' => "#^Method Nepada\\\\MessageBus\\\\Middleware\\\\LoggingMiddleware\\:\\:handle\\(\\) has Symfony\\\\Component\\\\Messenger\\\\Exception\\\\ExceptionInterface in PHPDoc @throws tag but it's not thrown\\.$#",
+        'path' => '../../src/MessageBus/Middleware/LoggingMiddleware.php',
+        'count' => 1,
+    ];
+}
 
 return $config;
